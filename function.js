20f96ef9706e677d9ed3f8acdc77b4eb7769ed00
Adrian Gee L. Mendio <mendio.andreagee@sfxc.edu.ph>
2024-05-26 13:20:32 +0800

    Updated HTML

--- function.js
+++ function.js
@@ -5,3 +5,16 @@ menuIcon.onclick = function(){
     sidebar.classList.toggle("small-sidebar");
     container.classList.toggle("large-container");
 }
+const searchBar = document.getElementById('search-bar');
+const searchIcon = document.querySelector('.search-box img');
+function performSearch(){
+    const query = searchBar.value.trim();
+   const videoPage = `html${query}.html`;
+   window.location.href = videoPage;
+}
+searchIcon.addEventListener('click', performSearch);
+searchBar.addEventListener('keydown', function(event) {
+    if(event.key === 'Enter'){
+        performSearch();
+    }
+})
